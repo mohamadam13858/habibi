76cc9b76015395800be716914f2f067e52daad15
habibi <mh711748@gmail.com>
2024-10-03 08:04:34 -0700

    warning

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+
+const addUserButton = document.getElementsByid("add_user_button")
+addUserButton.addEventListener("click" , ()=>{
+  document.querySelector(".add_user_modal").classList.add("show")
+ 
+})
+
   });
