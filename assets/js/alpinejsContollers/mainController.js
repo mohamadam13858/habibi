76ba0e42bc33c19900328e55726c6259ab3333b0
habibi <mh711748@gmail.com>
2024-10-07 06:18:44 -0700

    part9

--- assets/js/alpinejsContollers/mainController.js
+++
@@ -1,9 +0,0 @@
-
-    document.addEventListener('alpine:init', () => {
-        Alpine.data('', () => (
-            {message: 'I love Programming' , names {'qasem', 'abbas' , 'nima'} , testFunc: (){
-            
-                alert(this.message)
-            }
-        }))
-    })
